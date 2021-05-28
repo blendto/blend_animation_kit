978b631c11d272e935f809355df328dca4ab0847
Jamsheed Kamardeen <jamshyjamshy@gmail.com>
2021-05-28 21:56:56 +0530

    add /blend init api

---
+++ pages/api/blend.ts
@@ -0,0 +1,64 @@
+import { nanoid } from "nanoid";
+import DynamoDB from "server/external/dynamodb";
+import { DateTime } from "luxon";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      await initBlend(req, res);
+      break;
+    default:
+      res.status(500).json({ code: 500, message: "Something went wrong!" });
+  }
+};
+
+const initBlend = async (req: NextApiRequest, res: NextApiResponse) => {
+  let collabRequestId: String;
+
+  do {
+    collabRequestId = nanoid(8);
+    try {
+      const item = await DynamoDB.getItem({
+        TableName: process.env.BLEND_DYNAMODB_TABLE,
+        Key: {
+          id: collabRequestId,
+        },
+      });
+      if (!item) {
+        break;
+      }
+      continue;
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Something went wrong!" });
+      return;
+    }
+  } while (true);
+
+  const collab = {
+    id: collabRequestId,
+    status: "INITIALIZED",
+    statusUpdates: [
+      {
+        status: "INITIALIZED",
+        on: Date.now(),
+      },
+    ],
+    expireAt: DateTime.local().plus({ days: 1 }).startOf("second").toSeconds(),
+  };
+
+  try {
+    await DynamoDB.putItem({
+      TableName: process.env.BLEND_DYNAMODB_TABLE,
+      Item: collab,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+
+  res.send(collab);
+};
