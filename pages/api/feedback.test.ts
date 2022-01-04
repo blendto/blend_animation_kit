c8ec976b3163a5516031f0b80e057b6436e2ea3a
Jamsheed Kamardeen <jam@blend.to>
2022-01-04 13:00:51 +0530

    Feedback API (#25)
    * Feedback api
    
    * Added tests
    
    * add more tests
    
    * fix typo
    
    * delete unwanted file
    
    * add .vscode to .gitignore

---
+++ pages/api/feedback.test.ts
@@ -0,0 +1,82 @@
+import { pickAppropriateFeedbackOptions } from "./feedback";
+
+const data = {
+  en_US: {
+    options: [
+      {
+        id: "speed",
+        text: "Improove Speed",
+      },
+      {
+        id: "templates",
+        text: "Add More Templates",
+      },
+      {
+        id: "editor",
+        text: "Make Editor Easy",
+      },
+    ],
+  },
+  fr_FR: {
+    options: [
+      {
+        id: "speed",
+        text: "Améliorer la vitesse",
+      },
+      {
+        id: "templates",
+        text: "Ajouter plus de modèles",
+      },
+      {
+        id: "editor",
+        text: "Rendre l'éditeur facile",
+      },
+    ],
+  },
+  ml: {
+    options: [
+      {
+        id: "speed",
+        text: "വേഗത മെച്ചപ്പെടുത്തേണ്ടതുണ്ട്",
+      },
+      {
+        id: "templates",
+        text: "കൂടുതൽ ടെംപ്ലേറ്റുകൾ ചേർക്കണം",
+      },
+      {
+        id: "editor",
+        text: "എഡിറ്റർ എളുപ്പമായിരിക്കണം",
+      },
+    ],
+  },
+};
+
+describe("pickAppropriateFeedbackOptions", () => {
+  it("returns the right one when a specific locale is given", () => {
+    const englishOptions = pickAppropriateFeedbackOptions(data, "en_US");
+    expect(englishOptions).toMatchObject(data.en_US);
+
+    const mlOptions = pickAppropriateFeedbackOptions(data, "ml");
+    expect(mlOptions).toMatchObject(data.ml);
+  });
+
+  it("returns the right one when a - is given instead of _ in locale", () => {
+    const frenchOptions = pickAppropriateFeedbackOptions(data, "fr-FR");
+    expect(frenchOptions).toMatchObject(data.fr_FR);
+  });
+
+  it("fallbacks to default when no locale is given", () => {
+    const options = pickAppropriateFeedbackOptions(data, null);
+    expect(options).toMatchObject(data.en_US);
+  });
+
+  it("fallbacks to default when a random non existand locale is given", () => {
+    const options = pickAppropriateFeedbackOptions(data, "foo_Bar");
+    expect(options).toMatchObject(data.en_US);
+  });
+
+  it("fallbacks to language only when given country locale is not present", () => {
+    const options = pickAppropriateFeedbackOptions(data, "ml_IN");
+    expect(options).toMatchObject(data.ml);
+  });
+});
---
+++ pages/api/feedback.ts
@@ -0,0 +1,63 @@
+import DynamoDB from "server/external/dynamodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const DEFAULT_LOCALE = "en_US";
+interface FeedbackConfigDBEntry {
+  key: string;
+  version: string;
+  data: object;
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      await getFeedbackOptions(req, res);
+      break;
+
+    default:
+      res.status(404).json({ code: 404, message: "Invalid request" });
+  }
+};
+
+const getFeedbackOptions = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const feedbackConfig = (await DynamoDB.getItem({
+    TableName: process.env.CONFIG_DYNAMODB_TABLE,
+    Key: {
+      key: "feedback",
+      version: "1",
+    },
+  })) as FeedbackConfigDBEntry;
+
+  const configInSpecificLanguage = pickAppropriateFeedbackOptions(
+    feedbackConfig.data,
+    req.headers["accept-language"]
+  );
+
+  res.send(configInSpecificLanguage);
+};
+
+export const pickAppropriateFeedbackOptions = (
+  feedbackData: object,
+  prefferedLangauge?: string
+) => {
+  let locale = prefferedLangauge ?? DEFAULT_LOCALE;
+
+  const [language, country] = locale.split(/[_-]/);
+
+  locale = [language, country].join("_");
+
+  if (locale in feedbackData) {
+    return feedbackData[locale];
+  }
+
+  if (language in feedbackData) {
+    return feedbackData[language];
+  }
+
+  return feedbackData[DEFAULT_LOCALE];
+};
