790d545bfc127dca821d5c75c771489526a4c2b8
svk014 <souvik@blend.to>
2022-02-22 13:30:55 +0530

    Cleanup unnecessary imports (#73)

--- server/types.ts
+++ server/types.ts
@@ -1,9 +1,3 @@
-import { BatchService } from "server/service/batch";
-import DynamoDB from "server/external/dynamodb";
-import { BlendService } from "server/service/blend";
-import { UploadService } from "server/service/upload";
-import { UserService } from "server/service/user";
-
 export const TYPES = {
   DynamoDB: Symbol.for("DynamoDB"),
   BatchService: Symbol.for("BatchService"),
