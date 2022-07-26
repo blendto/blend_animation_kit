742fe770ee49264ddbb917fa0b1ca6018ea03bc7
Afzal Shahul Hameed <45993339+afzalIbnSH@users.noreply.github.com>
2022-07-26 13:45:43 +0530

    Fix incomplete profiles (#204)
    - Allow user profile to be created pre-signup. Eventually add data available post sign up.
    - Handle creation-on-update. Eventually fix older profiles created in this manner.
    
    Also,
    - Don't generate referral id before sign up. If the user signs up with email later, we'd want to use the first 4 characters from his email as the referral id prefix.

--- server/service/user.test.ts
+++ server/service/user.test.ts
@@ -9,11 +9,13 @@ import { UserService } from "server/service/user";
 describe("UserService", () => {
   const userService = diContainer.get<UserService>(TYPES.UserService);
   const id = "uxFJ2pRfNeMtfOO1dH5UhHKQbah2";
+  const email = "engg@blend.to";
   const stripeCustomerId = "cus_4QE4bx4C5BVSrC";
-  const createdAt = 1646906641;
-  const updatedAt = 1646906641;
+  const createdAt = Date.now();
+  const updatedAt = createdAt;
   const userDoc: User = {
     id,
+    email,
     stripeCustomerId,
     socialHandles: {},
     createdAt,
@@ -24,6 +26,8 @@ describe("UserService", () => {
     },
     favouriteRecipes: [],
   };
+  const referralId = "mark7211";
+  const referralLink = "https://links.foo.bar/CM9E";
 
   afterEach(() => {
     jest.restoreAllMocks();
@@ -33,6 +37,9 @@ describe("UserService", () => {
     it("Supports updating stripe customer id", async () => {
       const modelUpdateMock = jest
         .spyOn(userService.repo, "update")
+        .mockResolvedValueOnce(userDoc);
+      jest
+        .spyOn(userService, "ensureProfileHasAllData")
         .mockResolvedValueOnce(userDoc);
 
       const jsonPatch = [
@@ -49,16 +56,8 @@ describe("UserService", () => {
     });
   });
 
-  describe("addReferralIdAndLink", () => {
-    const referralId = "mark7211";
-    const referralLink = "https://links.foo.bar/CM9E";
-    it("Generates and adds a unique id and corresponding link to profile", async () => {
-      const updatedDoc = {
-        ...userDoc,
-        referralId,
-        referralLink,
-      };
-
+  describe("generateReferralIdAndLink", () => {
+    it("Generates a unique id and corresponding link to profile", async () => {
       jest
         .spyOn(userService, "generateReferralId")
         .mockReturnValueOnce(referralId);
@@ -66,19 +65,13 @@ describe("UserService", () => {
       jest
         .spyOn(userService, "generateReferralLink")
         .mockResolvedValueOnce(referralLink);
-      jest.spyOn(userService.repo, "update").mockResolvedValueOnce(updatedDoc);
 
-      const updatedUserDoc = await userService.addReferralIdAndLink(userDoc);
-      expect(updatedUserDoc).toMatchObject(updatedDoc);
+      expect(
+        await userService.generateReferralIdAndLink(userDoc)
+      ).toMatchObject({ referralId, referralLink });
     });
 
     it("Generates different ids until it's not a duplicate", async () => {
-      const updatedDoc = {
-        ...userDoc,
-        referralId,
-        referralLink,
-      };
-
       const generateReferralIdMock = jest
         .spyOn(userService, "generateReferralId")
         .mockReturnValue(referralId);
@@ -90,11 +83,152 @@ describe("UserService", () => {
       jest
         .spyOn(userService, "generateReferralLink")
         .mockResolvedValueOnce(referralLink);
-      jest.spyOn(userService.repo, "update").mockResolvedValueOnce(updatedDoc);
 
-      const updatedUserDoc = await userService.addReferralIdAndLink(userDoc);
-      expect(updatedUserDoc).toMatchObject(updatedDoc);
+      expect(
+        await userService.generateReferralIdAndLink(userDoc)
+      ).toMatchObject({ referralId, referralLink });
       expect(generateReferralIdMock.mock.calls.length).toEqual(3);
+    });
+
+    it("Doesn't generate data if the user has no email nor phone number meaning they haven't signed up", async () => {
+      jest
+        .spyOn(userService, "generateReferralId")
+        .mockReturnValueOnce(referralId);
+      jest.spyOn(userService, "getWithReferralId").mockResolvedValueOnce(null);
+      jest
+        .spyOn(userService, "generateReferralLink")
+        .mockResolvedValueOnce(referralLink);
+
+      let email: string;
+      expect(
+        await userService.generateReferralIdAndLink({ ...userDoc, email })
+      ).toMatchObject({ referralId: undefined, referralLink: undefined });
+    });
+  });
+
+  describe("generateBaseDataChanges", () => {
+    const anonymousUserFirebaseResponse = {
+      email: undefined,
+      displayName: undefined,
+      phoneNumber: undefined,
+      photoURL: undefined,
+      createdAt,
+    };
+    const displayName = "Engineer Mahaan";
+    const phoneNumber = "+91 888 777 6666";
+    const signedUserFirebaseResponse = {
+      email,
+      displayName,
+      phoneNumber,
+      photoURL: undefined,
+      createdAt,
+    };
+    it("Generates JSON patch with all missing non-signup values for anonymous user", async () => {
+      jest
+        .spyOn(userService, "generateFirebaseData")
+        .mockResolvedValueOnce(anonymousUserFirebaseResponse);
+
+      expect(
+        await userService.generateBaseDataChanges({
+          id,
+          socialHandles: {},
+          updatedAt,
+        })
+      ).toMatchObject([
+        {
+          op: "add",
+          path: `/activitySummary`,
+          value: {
+            posts: 0,
+            shoutoutsReceived: 0,
+          },
+        },
+        {
+          op: "add",
+          path: `/createdAt`,
+          value: createdAt,
+        },
+        {
+          op: "add",
+          path: `/favouriteRecipes`,
+          value: [],
+        },
+      ]);
+    });
+
+    it("Generates JSON patch with all missing values for signed-up user", async () => {
+      jest
+        .spyOn(userService, "generateFirebaseData")
+        .mockResolvedValueOnce(signedUserFirebaseResponse);
+
+      expect(
+        await userService.generateBaseDataChanges({
+          id,
+          socialHandles: {},
+          updatedAt,
+        })
+      ).toMatchObject([
+        {
+          op: "add",
+          path: `/email`,
+          value: email,
+        },
+        {
+          op: "add",
+          path: `/name`,
+          value: displayName,
+        },
+        {
+          op: "add",
+          path: `/phone`,
+          value: phoneNumber,
+        },
+        {
+          op: "add",
+          path: `/activitySummary`,
+          value: {
+            posts: 0,
+            shoutoutsReceived: 0,
+          },
+        },
+        {
+          op: "add",
+          path: `/createdAt`,
+          value: createdAt,
+        },
+        {
+          op: "add",
+          path: `/favouriteRecipes`,
+          value: [],
+        },
+      ]);
+    });
+  });
+
+  describe("generateReferralDataChanges", () => {
+    it("Generates JSON patch with id and corresponding link", async () => {
+      jest
+        .spyOn(userService, "generateReferralId")
+        .mockReturnValueOnce(referralId);
+      jest.spyOn(userService, "getWithReferralId").mockResolvedValueOnce(null);
+      jest
+        .spyOn(userService, "generateReferralLink")
+        .mockResolvedValueOnce(referralLink);
+
+      expect(
+        await userService.generateReferralDataChanges(userDoc)
+      ).toMatchObject([
+        {
+          op: "add",
+          path: `/referralId`,
+          value: referralId,
+        },
+        {
+          op: "add",
+          path: `/referralLink`,
+          value: referralLink,
+        },
+      ]);
     });
   });
 });
--- server/service/user.ts
+++ server/service/user.ts
@@ -12,9 +12,8 @@ import { inject, injectable } from "inversify";
 import IpApi from "server/external/ipapi";
 import { UserAgentDetails } from "server/base/models/userAgentDetails";
 import logger from "server/base/Logger";
-import { UserError } from "server/base/errors";
 import Firebase from "server/external/firebase";
-import { Repo } from "server/repositories/base";
+import { JSONPatch, Repo } from "server/repositories/base";
 import { UserUpdatePaths } from "server/repositories/user";
 import { UpdateOperations } from "server/repositories";
 import { SuggestionService } from "server/service/suggestion";
@@ -31,14 +30,111 @@ export class UserService implements IService {
   @inject(TYPES.UserRepo) repo: Repo<User>;
   ipApi = new IpApi();
 
+  private isAnonymous(profile: User): boolean {
+    return !profile.email && !profile.phone;
+  }
+
+  async generateFirebaseData(id: string): Promise<{
+    email: string;
+    displayName: string;
+    phoneNumber: string;
+    photoURL: string;
+    createdAt: number;
+  }> {
+    const firebaseRecord = await diContainer
+      .get<Firebase>(TYPES.Firebase)
+      .getUserById(id);
+    return {
+      email: firebaseRecord.email,
+      displayName: firebaseRecord.displayName,
+      phoneNumber: firebaseRecord.phoneNumber,
+      photoURL: firebaseRecord.photoURL,
+      createdAt: Date.parse(firebaseRecord.metadata.creationTime),
+    };
+  }
+
+  async generateBaseData(id: string): Promise<User> {
+    const { email, displayName, phoneNumber, photoURL, createdAt } =
+      await this.generateFirebaseData(id);
+    return {
+      id,
+      email,
+      name: displayName,
+      phone: phoneNumber,
+      socialHandles: {},
+      profilePicture: photoURL,
+      activitySummary: { posts: 0, shoutoutsReceived: 0 },
+      createdAt,
+      updatedAt: Date.now(),
+      favouriteRecipes: [],
+    };
+  }
+
+  async generateBaseDataChanges(profile: Partial<User>): Promise<JSONPatch> {
+    const baseData = await this.generateBaseData(profile.id);
+    const changes: JSONPatch = [];
+    Object.keys(baseData).forEach((attr) => {
+      if (baseData[attr] && !profile[attr]) {
+        changes.push({
+          op: "add",
+          path: `/${attr}`,
+          value: baseData[attr],
+        });
+      }
+    });
+    return changes;
+  }
+
+  async generateReferralDataChanges(profile: User): Promise<JSONPatch> {
+    const referralData = await this.generateReferralIdAndLink(profile);
+    return [
+      {
+        op: "add",
+        path: `/referralId`,
+        value: referralData.referralId,
+      },
+      {
+        op: "add",
+        path: `/referralLink`,
+        value: referralData.referralLink,
+      },
+    ];
+  }
+
+  async ensureProfileHasAllData(profile: User): Promise<User> {
+    // Base data will be definitely and only filled post sign-up
+    if (this.isAnonymous(profile)) {
+      const changes = await this.generateBaseDataChanges(profile);
+      if (changes.length) {
+        profile = await this.repo.update({ id: profile.id }, changes);
+      }
+    }
+    // Referral id will be generated only post sign-up
+    if (!profile.referralId && !this.isAnonymous(profile)) {
+      profile = await this.repo.update(
+        { id: profile.id },
+        await this.generateReferralDataChanges(profile)
+      );
+    }
+    // In cases of both sign up and referral data missing, it's easy to think that we could
+    // do with a single repo.update but really can't because referral data requires that sign up
+    // data be present and hence we must do one after the other.
+    return profile;
+  }
+
   async getOrCreate(id: string): Promise<User> {
     let profile = await this.repo.get({ id });
     if (!profile) {
-      profile = await this.populateUserFromFirebase(id);
+      profile = await this.create(id);
     }
-    if (!profile.referralId) {
-      profile = await this.addReferralIdAndLink(profile);
-    }
+
+    // Profiles could be missing partial/full data if it was
+    // - created before sign up. Firebase will not have returned any data.
+    // - created as part of an older update (when updates didn't care if a profile existed
+    //   but rather simply created a new record with the attributes passed to the update).
+    // - created before referral system was added.
+    profile = await this.ensureProfileHasAllData(profile);
+
     profile.favouriteRecipes = await this.fetchDetailedFavourites(
       profile.favouriteRecipes
     );
@@ -54,7 +150,12 @@ export class UserService implements IService {
   }
 
   async update(id: string, changes: UserJSONUpdate[]): Promise<User> {
-    const profile = await this.repo.update({ id }, changes);
+    let profile = await this.repo.update({ id }, changes);
+
+    // If a profile didn't exist at this point, this.repo.update would create
+    // entity with just the given attributes. Ensure the profile has all data.
+    profile = await this.ensureProfileHasAllData(profile);
+
     profile.favouriteRecipes = await this.fetchDetailedFavourites(
       profile.favouriteRecipes
     );
@@ -83,48 +184,31 @@ export class UserService implements IService {
       );
   }
 
-  async addReferralIdAndLink(profile: User): Promise<User> {
-    const referralId = this.generateReferralId(profile);
-    if (await this.getWithReferralId(referralId)) {
-      return await this.addReferralIdAndLink(profile);
+  async generateReferralIdAndLink(
+    profile: User
+  ): Promise<{ referralId: string; referralLink: string }> {
+    let referralId: string;
+    let referralLink: string;
+    if (this.isAnonymous(profile)) {
+      // Don't generate referral id before sign up. If the user signs up with email later,
+      // we'd want to use the first 4 character from his email as the referral id prefix.
+      return { referralId, referralLink };
     }
-    const referralLink = await this.generateReferralLink(referralId);
-    return await this.repo.update({ id: profile.id }, [
-      {
-        op: "add",
-        path: "/referralId",
-        value: referralId,
-      },
-      {
-        op: "add",
-        path: "/referralLink",
-        value: referralLink,
-      },
-    ]);
+    referralId = this.generateReferralId(profile);
+    if (await this.getWithReferralId(referralId)) {
+      return await this.generateReferralIdAndLink(profile);
+    }
+    referralLink = await this.generateReferralLink(referralId);
+    return { referralId, referralLink };
   }
 
-  async populateUserFromFirebase(userId: string): Promise<User> {
-    const firebaseService = diContainer.get<Firebase>(TYPES.Firebase);
-    const userRecord = await firebaseService.getUserById(userId);
-    const { email, displayName, phoneNumber, uid, photoURL } = userRecord;
-
-    if (!email && !phoneNumber) {
-      throw new UserError("User not verified");
-    }
-
-    const newUser: User = {
-      id: uid,
-      email,
-      name: displayName,
-      phone: phoneNumber,
-      socialHandles: {},
-      profilePicture: photoURL,
-      activitySummary: { posts: 0, shoutoutsReceived: 0 },
-      createdAt: Date.parse(userRecord.metadata.creationTime),
-      updatedAt: Date.now(),
-      favouriteRecipes: [],
+  async create(userId: string): Promise<User> {
+    let profile = await this.generateBaseData(userId);
+    profile = {
+      ...profile,
+      ...(await this.generateReferralIdAndLink(profile)),
     };
-    return await this.repo.create(newUser);
+    return await this.repo.create(profile);
   }
 
   async migrateUserBlends(
@@ -144,13 +228,13 @@ export class UserService implements IService {
     id: string,
     favourites: FavouriteRecipe[]
   ): Promise<User> {
-    const profile = await this.repo.update({ id }, [
-      { path: "/favouriteRecipes", op: "replace", value: favourites },
+    return await this.update(id, [
+      {
+        path: UserUpdatePaths.favouriteRecipes,
+        op: UpdateOperations.replace,
+        value: favourites,
+      },
     ]);
-    profile.favouriteRecipes = await this.fetchDetailedFavourites(
-      profile.favouriteRecipes
-    );
-    return profile;
   }
 
   async getUserAgent(ip: string): Promise<UserAgentDetails | null> {
