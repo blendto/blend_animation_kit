5d044dd136120f9f265ce83a7adf9008a4f2c689
Afzal Shahul Hameed <45993339+afzalIbnSH@users.noreply.github.com>
2022-08-02 20:10:55 +0530

    Put back "Fix incomplete profiles (#204)" (#215)
    It didn't cause the "Inaccessible blends" issue as we suspected.

--- server/repositories/user.ts
+++ server/repositories/user.ts
@@ -11,6 +11,7 @@ import { User } from "server/base/models/user";
 
 export enum UserUpdatePaths {
   stripeCustomerId = "/stripeCustomerId",
+  favouriteRecipes = "/favouriteRecipes",
 }
 
 const userDynamooseSchema = new DynamooseSchema({
