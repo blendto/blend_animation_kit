4d85faf68c23012372693796a7d89595464b6606
Afzal Shahul Hameed <45993339+afzalIbnSH@users.noreply.github.com>
2023-07-19 20:03:09 +0530

    Optimize catalogue images (#535)

--- pages/api/image/convert.ts
+++ pages/api/image/convert.ts
@@ -12,7 +12,10 @@ import { BlendMicroServices } from "../../../server/internal/inter-service-auth"
 import { MethodNotAllowedError, UserError } from "../../../server/base/errors";
 import { VALID_UPLOAD_IMAGE_EXTENSIONS } from "../../../server/helpers/constants";
 import { sharpInstance } from "../../../server/helpers/sharpUtils";
-import { convertUnspportedFormatToWebp } from "../../../server/helpers/imageUtils";
+import {
+  convertUnspportedFormatToWebp,
+  rescaleImage,
+} from "../../../server/helpers/imageUtils";
 
 const readFile = promisify(fs.readFile);
 export default withReqHandler(
@@ -52,28 +55,27 @@ const convertIncomingImage = async (
   const fileName: string = uploadData.fields.fileName[0];
   const { path } = uploadData.files.file[0];
   const imageBuffer = await readFile(path);
-  let validImageBuffer: Buffer;
+  let converted: Buffer;
   const fileNameArr: string[] = fileName.split(".");
   if (fileNameArr.length <= 1) {
     // no extension. Try to convert using sharp and hope for the best
-    validImageBuffer = await (await sharpInstance(imageBuffer))
+    converted = await (await sharpInstance(imageBuffer))
       .toFormat("webp")
       .toBuffer();
   } else {
     const fileExtension = fileNameArr.pop();
     if (VALID_UPLOAD_IMAGE_EXTENSIONS.includes(fileExtension)) {
-      validImageBuffer = await (
-        await sharpInstance(imageBuffer, {}, fileExtension)
-      )
+      converted = await (await sharpInstance(imageBuffer, {}, fileExtension))
         .toFormat("webp")
         .toBuffer();
     } else {
-      validImageBuffer = await convertUnspportedFormatToWebp(
-        imageBuffer,
-        fileName
-      );
+      converted = await convertUnspportedFormatToWebp(imageBuffer, fileName);
     }
   }
+  const optimized = await rescaleImage(converted, {
+    width: 720,
+    withoutEnlargement: true,
+  });
   res.setHeader("Content-Type", "image/webp");
-  return res.send(validImageBuffer);
+  return res.send(optimized);
 };
