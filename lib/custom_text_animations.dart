ef1ab107322b061c4729fb52e2d23188ea583161
deepak <deepak@blend.to>
2023-08-31 23:18:42 +0530

    use matrix4 decomposition

--- lib/custom_text_animations.dart
+++ lib/custom_text_animations.dart
@@ -1,4 +1,4 @@
 library custom_text_animations;
 
-export 'src/character_scale_fade_text_animation.dart';
+export 'src/character_text_animation.dart';
 export 'src/word_scale_text_animation.dart';
--- lib/src/character_text_animation.dart
+++ lib/src/character_text_animation.dart
@@ -1,29 +1,93 @@
+import 'dart:math';
+
 import 'package:custom_text_animations/src/helpers/flutter_sequence_animation/flutter_sequence_animation.dart';
 import 'package:custom_text_animations/src/helpers/flutter_sequence_animation/sequence_animation_tag.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' show Quaternion, Vector3;
 
-class CharacterScaleFadeTextAnimation extends StatefulWidget {
+class CharacterTextAnimation extends StatefulWidget {
   final String text;
   final TextStyle? textStyle;
 
-  final double initialCharacterScalingFactor;
+  final Matrix4 initialMatrix4;
+  final double initialCharacterOpacity;
   final Curve curve;
 
-  const CharacterScaleFadeTextAnimation({
+  final Duration characterAnimationSpeed;
+
+  final Duration Function(int index)? characterPaintDelay;
+
+  final Curve fadeOutCurve;
+
+  final Duration pauseDuration;
+  final Duration fadeOutDuration;
+
+  const CharacterTextAnimation({
     super.key,
     required this.text,
     this.textStyle,
-    this.initialCharacterScalingFactor = 4.0,
+    required this.initialMatrix4,
+    this.initialCharacterOpacity = 0.0,
     this.curve = Curves.easeOutExpo,
+    this.characterAnimationSpeed = const Duration(milliseconds: 950),
+    this.characterPaintDelay,
+    this.fadeOutCurve = Curves.easeOutExpo,
+    this.fadeOutDuration = const Duration(seconds: 1),
+    this.pauseDuration = const Duration(seconds: 1),
   });
 
+  Matrix4 get initialDefaultMatrix => Matrix4.identity()..scale(4.0, 4.0);
+
+  static CharacterTextAnimation variant1(String? text) {
+    return CharacterTextAnimation(
+      text: text ?? "Variant 1",
+      textStyle: const TextStyle(fontSize: 40),
+      curve: Curves.easeOutExpo,
+      initialMatrix4: Matrix4.identity()..scale(4.0),
+    );
+  }
+
+  static CharacterTextAnimation variant2(String? text) {
+    return CharacterTextAnimation(
+      text: text ?? "Variant 2",
+      textStyle: const TextStyle(fontSize: 40),
+      initialMatrix4: Matrix4.identity(),
+      characterAnimationSpeed: const Duration(milliseconds: 2250),
+      characterPaintDelay: (index) => Duration(milliseconds: 150 * (index + 1)),
+      curve: Curves.easeInOutQuad,
+    );
+  }
+
+  static CharacterTextAnimation variant3(String? text) {
+    return CharacterTextAnimation(
+      text: text ?? "Variant 3",
+      textStyle: const TextStyle(fontSize: 40),
+      initialMatrix4: Matrix4.identity()..scale(0.01),
+      curve: Curves.easeInOutQuad,
+      characterAnimationSpeed: const Duration(milliseconds: 1300),
+      characterPaintDelay: (index) => Duration(milliseconds: index * 45),
+    );
+  }
+
+  static CharacterTextAnimation variant4(String? text) {
+    return CharacterTextAnimation(
+      text: text ?? "Variant 4",
+      textStyle: const TextStyle(fontSize: 40),
+      initialMatrix4: Matrix4.identity()..rotateY(-pi / 2),
+      curve: Curves.easeOutExpo,
+      characterAnimationSpeed: const Duration(milliseconds: 1300),
+      characterPaintDelay: (index) => Duration(milliseconds: index * 45),
+    );
+  }
+
+  static List<Widget> all(String? text) =>
+      [variant1(text), variant2(text), variant3(text), variant4(text)];
+
   @override
-  State<CharacterScaleFadeTextAnimation> createState() =>
-      _CharacterScaleFadeTextAnimationState();
+  State<CharacterTextAnimation> createState() => _CharacterTextAnimationState();
 }
 
-class _CharacterScaleFadeTextAnimationState
-    extends State<CharacterScaleFadeTextAnimation>
+class _CharacterTextAnimationState extends State<CharacterTextAnimation>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
   late final SequenceAnimation sequenceAnimation;
@@ -35,7 +99,11 @@ class _CharacterScaleFadeTextAnimationState
   final charOpacityAnimations =
       SequenceAnimationTagList<double>(tagId: "char-opacity");
   final charScaleAnimations =
-      SequenceAnimationTagList<double>(tagId: "char-scaling");
+      SequenceAnimationTagList<Matrix4>(tagId: "char-scaling");
+  final charRotateAnimations =
+      SequenceAnimationTagList<Matrix4>(tagId: "char-rotation");
+  final charTranslateAnimations =
+      SequenceAnimationTagList<Matrix4>(tagId: "char-translate");
 
   @override
   void initState() {
@@ -43,6 +111,32 @@ class _CharacterScaleFadeTextAnimationState
     _controller = AnimationController(vsync: this);
     final animationBuilder = SequenceAnimationBuilder();
 
+    Vector3 translation = Vector3.zero();
+    Quaternion rotation = Quaternion.identity();
+    Vector3 scale = Vector3.zero();
+
+    widget.initialMatrix4.decompose(translation, rotation, scale);
+
+    final Matrix4Tween rotationMatrixTween = Matrix4Tween(
+        begin: Matrix4.identity()..setRotation(rotation.asRotationMatrix()),
+        end: Matrix4.identity());
+
+    final Matrix4Tween scaleMatrixTween = Matrix4Tween(
+      begin: Matrix4.identity()
+        ..setFromTranslationRotationScale(
+          Vector3.zero(),
+          Quaternion.identity(),
+          scale,
+        ),
+      end: Matrix4.identity(),
+    );
+
+    final Matrix4Tween translateMatrixTween = Matrix4Tween(
+      begin: Matrix4.identity()..setTranslation(translation),
+      end: Matrix4.identity(),
+    );
+
+    /// Point where all animations should start
     animationBuilder.addAnimatable(
       animatable: ConstantTween(0.0),
       from: Duration.zero,
@@ -52,39 +146,60 @@ class _CharacterScaleFadeTextAnimationState
 
     for (var (index, _) in widget.text.characters.indexed) {
       final opacityAnimationTag = charOpacityAnimations.addTag();
+      animationBuilder.addAnimatableAfterLastOneWithTag(
+        animatable: Tween(begin: widget.initialCharacterOpacity, end: 1.0),
+        tag: opacityAnimationTag,
+        delay: characterDelay(index),
+        duration: widget.characterAnimationSpeed,
+        curve: widget.curve,
+        lastTag: sequenceStartPositionTag,
+      );
       final scaleAnimationTag = charScaleAnimations.addTag();
+      animationBuilder.addAnimatableAfterLastOneWithTag(
+        animatable: scaleMatrixTween,
+        tag: scaleAnimationTag,
+        delay: characterDelay(index),
+        duration: widget.characterAnimationSpeed,
+        curve: widget.curve,
+        lastTag: sequenceStartPositionTag,
+      );
 
-      animationBuilder
-          .addAnimatableAfterLastOneWithTag(
-            animatable: Tween(begin: 0.0, end: 1.0),
-            tag: opacityAnimationTag,
-            delay: Duration(milliseconds: 70 * index),
-            duration: const Duration(milliseconds: 950),
-            curve: widget.curve,
-            lastTag: sequenceStartPositionTag,
-          )
-          .addAnimatableAfterLastOneWithTag(
-            animatable:
-                Tween(begin: widget.initialCharacterScalingFactor, end: 1.0),
-            tag: scaleAnimationTag,
-            delay: Duration(milliseconds: 70 * index),
-            duration: const Duration(milliseconds: 950),
-            curve: widget.curve,
-            lastTag: sequenceStartPositionTag,
-          );
+      final rotationAnimationTag = charRotateAnimations.addTag();
+      animationBuilder.addAnimatableAfterLastOneWithTag(
+        animatable: rotationMatrixTween,
+        tag: rotationAnimationTag,
+        delay: characterDelay(index),
+        duration: widget.characterAnimationSpeed,
+        curve: widget.curve,
+        lastTag: sequenceStartPositionTag,
+      );
+
+      final translateAnimationTag = charTranslateAnimations.addTag();
+      animationBuilder.addAnimatableAfterLastOneWithTag(
+        animatable: translateMatrixTween,
+        tag: translateAnimationTag,
+        delay: characterDelay(index),
+        duration: widget.characterAnimationSpeed,
+        curve: widget.curve,
+        lastTag: sequenceStartPositionTag,
+      );
     }
 
     sequenceAnimation = animationBuilder
         .addAnimatableAfterLastOne(
-          curve: widget.curve,
+          curve: widget.fadeOutCurve,
           animatable: Tween(begin: 1.0, end: 0.0),
-          duration: const Duration(seconds: 1),
-          delay: const Duration(seconds: 1),
+          duration: widget.fadeOutDuration,
+          delay: widget.pauseDuration,
           tag: fadeOutAnimationTag,
         )
         .animate(_controller);
     _controller.repeat();
   }
+
+  Duration characterDelay(int index) =>
+      widget.characterPaintDelay?.call(index) ??
+      Duration(milliseconds: 70 * index);
 
   @override
   void dispose() {
@@ -108,11 +223,21 @@ class _CharacterScaleFadeTextAnimationState
                     opacity: charOpacityAnimations
                         .getAnimation(sequenceAnimation, index)
                         .value,
-                    child: Transform.scale(
-                      scale: charScaleAnimations
+                    child: Transform(
+                      transform: charTranslateAnimations
                           .getAnimation(sequenceAnimation, index)
                           .value,
-                      child: Text(value, style: widget.textStyle),
+                      child: Transform(
+                        transform: charScaleAnimations
+                            .getAnimation(sequenceAnimation, index)
+                            .value,
+                        child: Transform(
+                          transform: charRotateAnimations
+                              .getAnimation(sequenceAnimation, index)
+                              .value,
+                          child: Text(value, style: widget.textStyle),
+                        ),
+                      ),
                     ),
                   ),
                 );
