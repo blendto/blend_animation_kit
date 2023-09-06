74a4d97476872b25c962bd139a4ff88a457f266c
deepak <deepak@blend.to>
2023-09-06 20:20:14 +0530

    test screenshots in flutter test

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -5,26 +5,49 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:example/main.dart';
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  await TestWidgetsFlutterBinding.instance.runAsync(() async {
+    final fontData = File('test/Roboto-Regular.ttf')
+        .readAsBytes()
+        .then((bytes) => ByteData.view(Uint8List.fromList(bytes).buffer));
+    final fontLoader = FontLoader('Roboto')..addFont(fontData);
+    await fontLoader.load();
+    return true;
   });
+
+  final GlobalKey key = GlobalKey();
+  runApp(MaterialApp(
+    home: Material(
+      child: RepaintBoundary(
+        key: key,
+        child: const Text(
+          "Hello World",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  ));
+  final boundary =
+      key.currentContext?.findRenderObject() as RenderRepaintBoundary?;
+  final image = await boundary?.toImage();
+  final byteData = await image?.toByteData(format: ImageByteFormat.png);
+  image?.dispose();
+  final imageBytes = byteData?.buffer.asUint8List();
+  final file = await TestWidgetsFlutterBinding.instance.runAsync(() async {
+    return await File('img.png').writeAsBytes(imageBytes!.toList());
+  });
+  markTestSkipped("Saved $file");
 }
