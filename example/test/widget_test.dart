06e265cfbed1d43c167a813aa55a25e9bcf19428
deepak <deepak@blend.to>
2023-09-07 18:57:40 +0530

    add server[wi[]

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,20 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'dart:async';
 import 'dart:io';
-import 'dart:ui';
+import 'dart:ui' as ui;
 
 import 'package:custom_text_animations/custom_text_animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
-const fps = 24;
+const fps = 60;
 const paddingLength = 5;
 
 TextAnimationBuilder testAnimation(String text, TextStyle? textStyle) =>
@@ -32,80 +28,93 @@ TextAnimationBuilder testAnimation(String text, TextStyle? textStyle) =>
         )
         .waitAndFadeOutAll();
 
+Future<Response> _echoRequest(Request request, WidgetTester tester) async {
+  final stop = Stopwatch()..start();
+  final file = await pumpAndGenerate(request.url.toString(), tester);
+  print("gen ${stop.elapsedMilliseconds}");
+  return Response.ok("file.readAsBytes().asStream()",
+      headers: {"Content-Type": "video/webm"});
+}
+
 void main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
 
+  await TestWidgetsFlutterBinding.instance.runAsync(() async {
+    final fontData = File('test/Roboto-Regular.ttf')
+        .readAsBytes()
+        .then((bytes) => ByteData.view(Uint8List.fromList(bytes).buffer));
+    final fontLoader = FontLoader('Roboto')..addFont(fontData);
+    await fontLoader.load();
+    return true;
+  });
   testWidgets("Some  test", (tester) async {
-    await TestWidgetsFlutterBinding.instance.runAsync(() async {
-      final fontData = File('test/Roboto-Regular.ttf')
-          .readAsBytes()
-          .then((bytes) => ByteData.view(Uint8List.fromList(bytes).buffer));
-      final fontLoader = FontLoader('Roboto')..addFont(fontData);
-      await fontLoader.load();
-      return true;
+    final server = await TestWidgetsFlutterBinding.instance.runAsync(() async {
+      var handler = const Pipeline()
+          .addMiddleware(logRequests())
+          .addHandler((req) => _echoRequest(req, tester));
+      return await shelf_io.serve(handler, 'localhost', 8080);
     });
-
-    final stop = Stopwatch()..start();
-    final testAnimationBuilder = testAnimation(
-        "Hello World",
-        const TextStyle(
-          fontSize: 30,
-          color: Colors.red,
-        ));
-    final GlobalKey key = GlobalKey();
-    await tester.pumpWidget(MaterialApp(
-      home: Material(
-        child: RepaintBoundary(
-          key: key,
-          child: testAnimationBuilder.generateWidget(),
-        ),
-      ),
-    ));
-
-    int index = 0;
-    final perFrameDuration = Duration(milliseconds: (1000 / (fps)).round());
-
-    Duration currentDuration = Duration.zero;
-    List<FrameItem> frames = [];
-    await TestWidgetsFlutterBinding.instance.runAsync(() async {
-      while (currentDuration <= testAnimationBuilder.tween.duration) {
-        final boundary =
-            key.currentContext?.findRenderObject() as RenderRepaintBoundary?;
-        final image = await boundary?.toImage();
-        final byteData = await image?.toByteData(format: ImageByteFormat.png);
-        image?.dispose();
-        frames
-            .add(FrameItem(imageBuffer: byteData!, duration: currentDuration));
-        await File(
-                "./captures/img-${index.toString().padLeft(paddingLength, '0')}.png")
-            .writeAsBytes(byteData.buffer.asUint8List().toList());
-        currentDuration += perFrameDuration;
-        index += 1;
-        await TestWidgetsFlutterBinding.instance.pump(perFrameDuration);
-      }
-    });
-
-    print(stop.elapsedMilliseconds);
-    final args = <String>[
-      "-r",
-      fps.toString(),
-      "-y",
-      "-i",
-      "./captures/img-%0${paddingLength}d.png",
-      "./captures/output.webm"
-    ];
-    await TestWidgetsFlutterBinding.instance.runAsync(() async {
-      var result = await Process.run('ffmpeg', args);
-      print(result.stderr);
-      print(result.stdout);
-    });
-    print(stop.elapsedMilliseconds);
+    await Completer().future;
   });
 }
 
+Future<File> pumpAndGenerate(String text, WidgetTester tester) async {
+  final testAnimationBuilder = testAnimation(
+      text,
+      const TextStyle(
+        fontSize: 30,
+        color: Colors.red,
+      ));
+  final GlobalKey key = GlobalKey();
+  await tester.pumpWidget(MaterialApp(
+    home: Material(
+      child: RepaintBoundary(
+        key: key,
+        child: testAnimationBuilder.generateWidget(),
+      ),
+    ),
+  ));
+
+  int index = 0;
+  final perFrameDuration = Duration(milliseconds: (1000 / (fps)).round());
+
+  Duration currentDuration = Duration.zero;
+  final boundary =
+      key.currentContext?.findRenderObject() as RenderRepaintBoundary;
+
+  final frames = Frames();
+  while (currentDuration <= (testAnimationBuilder.tween.duration)) {
+    final image = await boundary.toImage();
+    frames.add(FrameItem(image: image, duration: currentDuration));
+    currentDuration += perFrameDuration;
+    index += 1;
+    await TestWidgetsFlutterBinding.instance.pump(perFrameDuration);
+  }
+
+  const filePath = "./captures/output.webm";
+  // final args = <String>[
+  //   "-r",
+  //   fps.toString(),
+  //   "-y",
+  //   "-i",
+  //   "./captures/img-%0${paddingLength}d.png",
+  //   filePath
+  // ];
+  // var result = await Process.run('ffmpeg', args);
+  return File(filePath);
+}
+
 class FrameItem {
-  final ByteData imageBuffer;
+  final ui.Image image;
   final Duration duration;
 
-  const FrameItem({required this.imageBuffer, required this.duration});
+  const FrameItem({required this.image, required this.duration});
+}
+
+class Frames {
+  final List<FrameItem> frames = [];
+
+  void add(FrameItem frameItem) {
+    return frames.add(frameItem);
+  }
 }
